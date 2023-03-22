22a4fa35a630686c7c8d461cfe3fc1ae9cd8dfe6
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-03-22 11:44:33 -0300

    galeria de guitarras js

--- js/script.js
+++ js/script.js
@@ -44,3 +44,22 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de Guitarras
+
+const galeria = document.querySelectorAll('.guitarra-imagens img');
+const galeriaContainer = document.querySelector('.guitarra-imagens');
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia('(min-width: 1000px)').matches;    
+    if(media) {
+        galeriaContainer.prepend(img);
+    }            
+}
+
+function eventosGaleria(img) {
+    img.addEventListener('click', trocarImagem)
+}
+
+galeria.forEach(eventosGaleria);
