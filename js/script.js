8330eacfd92c0e67b93c4e71babf2e7580edabbf
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-03-21 21:23:05 -0300

    itens do orçamento js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Ativar Links do Menu
+
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link) {
@@ -9,3 +11,16 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar Itens do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if(elemento) {
+        elemento.checked = true; 
+    } 
+}
+
+parametros.forEach(ativarProduto)
