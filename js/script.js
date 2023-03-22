22a7ada092d043c263436d68ab36e3de1820e234
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-03-22 09:33:12 -0300

    perguntas frequentes js

--- js/script.js
+++ js/script.js
@@ -23,4 +23,24 @@ function ativarProduto(parametro) {
     } 
 }
 
-parametros.forEach(ativarProduto)
+parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(controls);
+    
+    resposta.classList.toggle('ativa');
+    const ativa = resposta.classList.contains('ativa');   
+    pergunta.setAttribute('aria-expanded', ativa);    
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
